675d267f300d16886eb42df081aee39df8e71337
NatalyaZinger <nata.zinger@gmail.com>
2022-09-03 11:50:33 +0500

    final

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -12,9 +12,19 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 26, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 26;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+
 }
